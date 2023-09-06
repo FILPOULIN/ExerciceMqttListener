5b145bf92e5590857f6e90f553785eb7552491ef
FILPOULIN <2037339@cegepba.qc.ca>
2023-09-06 12:06:41 -0400

    Final

--- main.py
+++ main.py
@@ -6,18 +6,16 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("jacob/temperature/sensor")
-
-# The callback for when a PUBLISH message is received from the server.
+    client.subscribe("XX/temp/sensor")
+    # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(str(msg.payload.decode()) + "°C")
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("10.4.1.42", 1883, 60)
-
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
